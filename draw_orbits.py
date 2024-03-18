5233961df05487e0089815ab00b5c88c17c0b651
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2024-03-18 10:03:22 -0500

    revert change to draw-orbits

--- draw_orbits.py
+++ draw_orbits.py
@@ -2,7 +2,7 @@
 
 import physics_simulation as sim
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import matplotlib.animation as animation  
 import json
 import numpy as np
 
@@ -16,45 +16,44 @@ MAX_T = 31536000
 
 fig, ax = plt.subplots()
 
-# ax.set_xlim([-258000000000, 258000000000])
-# ax.set_ylim([-258000000000, 258000000000])
+ax.set_xlim([-258000000000, 258000000000])
+ax.set_ylim([-258000000000, 258000000000]) 
 
 scatter = ax.scatter([], [])
 
-
 # called for frame i
 def animate(i):
-    # t is a parameter which varies with the frame number
+    # t is a parameter which varies with the frame number 
     # 2 days per frame
-    # t = 2 * SECONDS_IN_DAY * i
+    t =  2 * SECONDS_IN_DAY * i  
 
     # slow down timer to see moon orbit earth
-    t = 3600 * i
+    # t = 3600 * i
 
     x_data = []
     y_data = []
-
+    
     # grab the data from physics_simulation.py
     data = sim.get_stats(t)
-
-    for datum in data:
-        # appending values to the previously
-        # empty x and y data holders
-        x_data.append(datum["x"])
+    
+    for datum in data:       
+        # appending values to the previously  
+        # empty x and y data holders  
+        x_data.append(datum["x"])  
         y_data.append(datum["y"])
 
     # uncomment to see moon orbit earth
-    ax.set_xlim([data[1]["x"] - EARTH_ORBIT_RAD, data[1]["x"] + EARTH_ORBIT_RAD])
-    ax.set_ylim([data[1]["y"] - EARTH_ORBIT_RAD, data[1]["y"] + EARTH_ORBIT_RAD])
+    # ax.set_xlim([data[1]["x"] - EARTH_ORBIT_RAD, data[1]["x"] + EARTH_ORBIT_RAD])
+    # ax.set_ylim([data[1]["y"] - EARTH_ORBIT_RAD, data[1]["y"] + EARTH_ORBIT_RAD]) 
 
     # add it to animation
-    scatter.set_offsets(np.column_stack((x_data, y_data)))
-    return (scatter,)
+    scatter.set_offsets(np.column_stack((x_data, y_data)))  
+    return scatter,
 
 
 # bootstrap for animated python file
 def animated():
-    anim = animation.FuncAnimation(fig, animate, frames=5000, interval=20, blit=True)
+    anim = animation.FuncAnimation(fig, animate, frames = 5000, interval = 20, blit = True)
     plt.show()
 
 
@@ -70,15 +69,17 @@ def daily_points():
         data = sim.get_stats(t)
         index = 0
 
+        
         plt.xlim(-SOLAR_SYSTEM_RAD, SOLAR_SYSTEM_RAD)
         plt.ylim(-SOLAR_SYSTEM_RAD, SOLAR_SYSTEM_RAD)
-
+        
         for datam in data:
             index = (index + 1) % len(colors)
-            plt.plot(datam["x"], datam["y"], colors[index] + "o")
+            plt.plot(datam["x"], datam["y"], colors[index] + 'o')
+            
 
         t += SECONDS_IN_DAY
         plt.show()
-
+    
 
 animated()
