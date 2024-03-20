8dc2a6391caf1dab34db9ea3a92a8ce1b7675c6a
lysac <lysackm5@gmail.com>
2024-03-19 21:18:56 -0500

    added names

--- physics_simulation.py
+++ physics_simulation.py
@@ -29,6 +29,7 @@ C = 299792458.0
 # randomize initial positions
 
 # id: the id for the entity
+# name: the english name of the entity
 # or: orbital radius of the entity (m)
 # orb_id: the id of the entity which this entity orbits
 #         The sun will be set to -1 (no index), since it is not orbiting any modeled entities
@@ -39,6 +40,7 @@ C = 299792458.0
 initial_pos = [
     { # Sun
         "id": 0,
+        "name": "Sun",
         "orbital_radius": 0,
         "orb_id": -1,
         "period": 1,
@@ -47,6 +49,7 @@ initial_pos = [
         "orbital_direction": 1,
     },{ # Earth
         "id": 1,
+        "name": "Earth",
         "orbital_radius": 149600000000,
         "orb_id": 0,
         "period": 31558149,
@@ -55,6 +58,7 @@ initial_pos = [
         "orbital_direction": 1,
     },{ # Mars
         "id": 2,
+        "name": "Mars",
         "orbital_radius": 228000000000,
         "orb_id": 0,
         "period": 59355072,
@@ -63,6 +67,7 @@ initial_pos = [
         "orbital_direction": 1,
     },{ # Moon
         "id": 3,
+        "name": "The Moon",
         "orbital_radius": 385000000,
         "orb_id": 1,
         "period": 361592,
@@ -71,6 +76,7 @@ initial_pos = [
         "orbital_direction": 1,
     },{ # ISS
         "id": 4,
+        "name": "ISS",
         "orbital_radius": 6371000 + 400000, # Earth radius + 400km
         "orb_id": 1,
         "period": 5580,
@@ -79,6 +85,7 @@ initial_pos = [
         "orbital_direction": 1,
     },{ # Mars Orbiter
         "id": 5,
+        "name": "Mars Orbiter",
         "orbital_radius": 3389500 + 400000, # Mars radius + 400km
         "orb_id": 2,
         "period": 7200,
@@ -127,6 +134,7 @@ def get_entity_stats(t, entity_stats, stat_list):
 
     return {
         "id": entity_stats["id"],
+        "name": entity_stats["name"],
         "orbital_radius": entity_stats["orbital_radius"],
         "orb_id": entity_stats["orb_id"],
         "period": entity_stats["period"],
@@ -149,6 +157,7 @@ def get_stats(t: int):
             stats.append(
                 {
                     "id": 0,
+                    "name": "Sun",
                     "orbital_radius": 0,
                     "orb_id": -1,
                     "period": 1,
@@ -212,6 +221,7 @@ def point_dist_to_line(line_x1: float, line_y1: float, line_x2: float, line_y2: 
 # takes in a data structure stats which has all of the meta data for all of the 
 # entities, including:
 # "id": id of the entity
+# "name": english name of the entity
 # "orbital_radius": radius of the orbit (m)
 # "orb_id": id of the entity which this entity orbits
 # "period": orbital period (s)
@@ -252,6 +262,7 @@ def get_connections(stats):
                 entity_sending["connections"].append(
                     {
                         "id": entity_receiving["id"],
+                        "name": entity_receiving["name"],
                         "connected": not blocking,
                         "distance": dist,
                         "trans_time": trans_time,
